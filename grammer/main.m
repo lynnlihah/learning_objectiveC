8c7f9455876e983efdb735f156cfe4dc5f7f1939
lizhufeng <lizhufeng@zdns.cn>
2017-10-09 17:32:58 +0800

    manage main.m

--- grammer/main.m
+++ grammer/main.m
@@ -12,30 +12,45 @@
 #import "Calculator.h"
 #import "BaseGrammer.h"
 
+void callFractionClass();
+void callCalculationClass();
+
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
         // insert code here...
-        Calculator *deskCalc = [[Calculator alloc] init];
-
-        [deskCalc setAccumulator:100.0];
-        [deskCalc add: 200.];
-        [deskCalc divide: 15.0];
-        [deskCalc substract: 10.];
-        [deskCalc multiply: 5];
-        NSLog(@"The result is %g", [deskCalc accumulator]);
         NSLog(@"Hello, World!");
-        // insert code here...
-//        Fraction * fraction = [[Fraction alloc]init];
-//        [fraction setNumerator:10];
-//        NSLog(@"numerator is %d", [fraction numerator]);
-//        
-//        fraction.numerator = 20;
-//        NSLog(@"numerator is %d", fraction.numerator);
+
         NSLog(@"Type in your number:");
         int n;
         scanf("%i", &n);
         BaseGrammer *selstructure = [[BaseGrammer alloc] init];
         [selstructure if_structure: n];
+        
+//        callFractionClass();
+//        callCalculationClass();
     }
     return 0;
+}
+
+
+void callFractionClass()
+{
+    Fraction * fraction = [[Fraction alloc]init];
+    [fraction setNumerator:10];
+    NSLog(@"numerator is %d", [fraction numerator]);
+
+    fraction.numerator = 20;
+    NSLog(@"numerator is %d", fraction.numerator);
+}
+
+void callCalculationClass()
+{
+    Calculator *deskCalc = [[Calculator alloc] init];
+    
+    [deskCalc setAccumulator:100.0];
+    [deskCalc add: 200.];
+    [deskCalc divide: 15.0];
+    [deskCalc substract: 10.];
+    [deskCalc multiply: 5];
+    NSLog(@"The result is %g", [deskCalc accumulator]);
 }
